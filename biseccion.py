13a08391d34af835f74489a3caccc93c6b5ee0d2
ereyes <eduardo.reyeso.14@sansano.usm.cl>
2018-03-06 19:00:11 -0300

    Se soluciono el problema de división por zero y ahora se muestra un gráfico al ingresar la funcion para elegir el intervalo para hacer biseccion

--- biseccion.py
+++ biseccion.py
@@ -4,11 +4,12 @@ from sympy import *
 x = symbols("x")
 print "write your function,example x**4+3*x**3-2"
 function = raw_input('f(x)= ')
+expr = sympify(function)
+plot(expr,(x,-3,3))
 xa = float(raw_input('xa: '))
 xb = float(raw_input('xb: '))
 print "write your required error,example 0.001"
 required_error = float(raw_input('required error: '))
-expr = sympify(function)
 current_error = 99
 old_value = 0
 solution = 0
@@ -22,8 +23,12 @@ while(required_error<current_error):
     else:
         xb = xr
     if(old_value!=0):
-        current_error = abs((xr-old_value)/xr)
-        solution = xr
+        if(xr!=0):
+            current_error = abs((xr-old_value)/xr)
+            solution = xr
+        else:
+            solution = 0
+            current_error=0
     old_value = xr
 print "The solution is: "
 print solution
