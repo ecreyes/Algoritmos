8a77f313bff0b4a34d8006bd9ee6538e5ae61b97
ereyes <eduardo.reyeso.14@sansano.usm.cl>
2018-03-06 14:39:34 -0300

    Método de bisección creado en python2.7 y sympy

---
+++ biseccion.py
@@ -0,0 +1,29 @@
+#Eduardo Reyes Oyarzo
+#python 2.7
+from sympy import *
+x = symbols("x")
+print "write your function,example x**4+3*x**3-2"
+function = raw_input('f(x)= ')
+xa = float(raw_input('xa: '))
+xb = float(raw_input('xb: '))
+print "write your required error,example 0.001"
+required_error = float(raw_input('required error: '))
+expr = sympify(function)
+current_error = 99
+old_value = 0
+solution = 0
+
+while(required_error<current_error):
+    xr = (xa+xb)/2
+    fxa = expr.subs(x,xa)
+    fxr = expr.subs(x,xr)
+    if(fxa*fxr>0):
+        xa = xr
+    else:
+        xb = xr
+    if(old_value!=0):
+        current_error = abs((xr-old_value)/xr)
+        solution = xr
+    old_value = xr
+print "The solution is: "
+print solution
